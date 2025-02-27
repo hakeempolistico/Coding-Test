3b4a1966e308f53362605353ec522d38209e7438
Hakeem Polistico <hjpolistico@gmail.com>
2025-02-27 14:08:50 +0800

    fix(page): fixed not found page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,17 +1,19 @@
-export default function NotFound() {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br text-orange-500">
-            <h1 className="text-7xl font-extrabold">404</h1>
-            <p className="text-2xl mt-2 font-semibold">Oops! Page not found.</p>
-            <p className="mt-2 text-lg">The page you are looking for doesn't exist.</p>
+import Link from "next/link";
 
-            <a
-                href="/"
-                className="mt-6 px-6 py-3 bg-white text-orange-500 rounded-lg shadow-md font-semibold text-lg 
-                     hover:bg-gray-100 transition-all"
-            >
-                Go Back Home
-            </a>
-        </div>
-    );
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-yellow-300 to-orange-500 text-white">
+      <h1 className="text-7xl font-extrabold">404</h1>
+      <p className="text-2xl mt-2 font-semibold">Oops! Page not found.</p>
+      <p className="mt-2 text-lg">The page you are looking for doesn&apos;t exist.</p>
+
+      <Link
+        href="/"
+        className="mt-6 px-6 py-3 bg-white text-orange-500 rounded-lg shadow-md font-semibold text-lg 
+                   hover:bg-gray-100 transition-all"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
 }
