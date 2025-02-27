2b26aa14900a36566dede7ecda414c16e74c1c78
Hakeem Polistico <hjpolistico@gmail.com>
2025-02-27 14:14:22 +0800

    fix(404): fixed 404 layout

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-yellow-300 to-orange-500 text-white">
+    <div className="flex flex-col items-center justify-center h-screen text-orange-500">
       <h1 className="text-7xl font-extrabold">404</h1>
       <p className="text-2xl mt-2 font-semibold">Oops! Page not found.</p>
       <p className="mt-2 text-lg">The page you are looking for doesn&apos;t exist.</p>
