11a8a9c80f6e1be0e6dc8dd4a80b3961b12087b1
Hakeem Polistico <hjpolistico@gmail.com>
2025-02-27 14:04:59 +0800

    feat(home): updated home page and 404

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,17 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br text-orange-500">
+            <h1 className="text-7xl font-extrabold">404</h1>
+            <p className="text-2xl mt-2 font-semibold">Oops! Page not found.</p>
+            <p className="mt-2 text-lg">The page you are looking for doesn't exist.</p>
+
+            <a
+                href="/"
+                className="mt-6 px-6 py-3 bg-white text-orange-500 rounded-lg shadow-md font-semibold text-lg 
+                     hover:bg-gray-100 transition-all"
+            >
+                Go Back Home
+            </a>
+        </div>
+    );
+}
